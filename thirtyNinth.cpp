dd35740c331a1c94d16d0899ca1107c9f0301349
Alind Sharma <alindsharmasimply@gmail.com>
2017-11-29 00:05:58 +0530

    Time complexity is a lot

--- thirtyNinth.cpp
+++ thirtyNinth.cpp
@@ -12,12 +12,22 @@ int main() {
     while(n--)
     {
         int k = 0;
-        string s, s1, s2;
+        string s1, s2;
         cin >> s1 >> s2;
         for (int i = 0; i < s1.length(); i++) {
             for (int j = 0; j < s2.length(); j++) {
-                
+                if (s1[i] == s2[j]) {
+                    k = 1;
+                    std::cout << "YES" << '\n';
+                    break;
+                }
             }
+            if (k == 1) {
+                break;
+            }
+        }
+        if (k == 0) {
+            std::cout << "NO" << '\n';
         }
     }
     return 0;
