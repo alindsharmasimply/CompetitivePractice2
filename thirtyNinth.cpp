698be50d808a6a9b97b608771de88b1fc015c80a
Alind Sharma <alindsharmasimply@gmail.com>
2017-11-28 23:59:53 +0530

    Easiest of all

---
+++ thirtyNinth.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int k = 0;
+        string s, s1, s2;
+        cin >> s1 >> s2;
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                
+            }
+        }
+    }
+    return 0;
+}
